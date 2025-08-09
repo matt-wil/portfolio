dea337ae2533d3689260924f05f6adaf0c567822
Matt Williams <matthew.steven.williams@gmail.com>
2025-08-09 22:56:53 +0200

    update for new anker project and remove zoom on globe

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,52 +1,49 @@
-import { OrbitControls } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
-import { Room } from "./Room.jsx"
-import { useMediaQuery } from "react-responsive"
-import HeroLights from "./HeroLights.jsx"
-import { LightBulb } from "./LightBulb.jsx"
-import { useRef } from "react"
-import { EffectComposer, Bloom } from "@react-three/postprocessing"
-
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Room } from "./Room.jsx";
+import { useMediaQuery } from "react-responsive";
+import HeroLights from "./HeroLights.jsx";
+import { LightBulb } from "./LightBulb.jsx";
+import { useRef } from "react";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
 
 const HeroExperience = () => {
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
-  const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
   const groupRef = useRef();
-
 
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
       <EffectComposer>
-      <Bloom 
-        luminanceThreshold={0.3}
-        luminanceSmoothing={0.9}
-        intensity={.5}
-      />
-      { !isMobile &&
-        <OrbitControls 
-          enablePan={false}
-          enableZoom={!isTablet}
-          maxDistance={20}
-          minDistance={5}
-          minPolarAngle={Math.PI / 5}
-          maxPolarAngle={Math.PI / 2}
-        />  
-      }
+        <Bloom
+          luminanceThreshold={0.3}
+          luminanceSmoothing={0.9}
+          intensity={0.5}
+        />
+        {!isMobile && (
+          <OrbitControls
+            enablePan={false}
+            enableZoom={false}
+            maxDistance={20}
+            minDistance={5}
+            minPolarAngle={Math.PI / 5}
+            maxPolarAngle={Math.PI / 2}
+          />
+        )}
         <HeroLights />
-        <group 
+        <group
           ref={groupRef}
-          scale={isMobile? 1.7 : 2}
+          scale={isMobile ? 1.7 : 2}
           position={[0, -3.5, 0]}
-          rotation={[0, -Math.PI /4, 0]}
+          rotation={[0, -Math.PI / 4, 0]}
         >
-          <LightBulb isMobile={isMobile}/>
-          {/** 
+          <LightBulb isMobile={isMobile} />
+          {/**
           <Room />
           */}
         </group>
-        </EffectComposer>
+      </EffectComposer>
     </Canvas>
-  )
-}
+  );
+};
 
-export default HeroExperience
+export default HeroExperience;
--- src/sections/Showcase.jsx
+++ src/sections/Showcase.jsx
@@ -6,79 +6,100 @@ import { useGSAP } from "@gsap/react";
 gsap.registerPlugin(ScrollTrigger);
 
 const Showcase = () => {
-    const sectionRef = useRef(null);
-    const project1Ref = useRef(null);
-    const project2Ref = useRef(null);
-    const project3Ref = useRef(null);
+  const sectionRef = useRef(null);
+  const project1Ref = useRef(null);
+  const project2Ref = useRef(null);
+  const project3Ref = useRef(null);
 
-    
-    
-    useGSAP(() => {
-        const projects = [project1Ref.current, project2Ref.current, project3Ref.current];
-        projects.forEach((card, index) => {
-            gsap.fromTo(
-                card, {y: 50, opacity: 0}, 
-                {y:0, opacity:1, duration: 1, delay: 0.3 * (index +1),
-                    scrollTrigger: {
-                        trigger: card,
-                        start: "top bottom"
-                    }
-                }
-            )
-        })
-        gsap.fromTo(sectionRef.current, 
-            {opacity: 0}, // from
-            {opacity: 1, duration: 1.5}  // to
-    )
-    }, [])
+  useGSAP(() => {
+    const projects = [
+      project1Ref.current,
+      project2Ref.current,
+      project3Ref.current,
+    ];
+    projects.forEach((card, index) => {
+      gsap.fromTo(
+        card,
+        { y: 50, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          delay: 0.3 * (index + 1),
+          scrollTrigger: {
+            trigger: card,
+            start: "top bottom",
+          },
+        },
+      );
+    });
+    gsap.fromTo(
+      sectionRef.current,
+      { opacity: 0 }, // from
+      { opacity: 1, duration: 1.5 }, // to
+    );
+  }, []);
 
   return (
     <section id="work" ref={sectionRef} className="app-showcase">
-    <div className="w-full">
+      <div className="w-full">
         <div className="showcaselayout">
-            {/** LEFT */}
-            <div className="first-project-wrapper" ref={project1Ref}>
-                <div className="image-wrapper">
-                    <img className="" src="/images/project1.png" alt="Anker Website"/>
-                </div>
-                <div className="text-content">
-                    <h2>Business Website with a Simple yet Elegant Design and Focussed on Ease of Use.</h2>
-                    <p className="text-white-50 text-xl">
-                        A Website Built with Vite, React, Tailwind CSS, GSAP, Python, SQL and i18next. (Test Phase Deployment)
-                    </p>
-                    <p>
-                        <a href="https://anker-tattoo-piercing.vercel.app/" target="_blank">Check it out!</a>
-                    </p>
-                </div>
+          {/** LEFT */}
+          <div className="first-project-wrapper" ref={project1Ref}>
+            <div className="image-wrapper">
+              <img
+                className=""
+                src="/images/project1.png"
+                alt="Anker Website"
+              />
             </div>
-            {/** RIGHT */}
-            <div className="project-list-wrapper overflow-hidden">
-                <div className="project" ref={project2Ref}>
-                    <div className="image-wrapper bg-[rgb(23,23,25)]">
-                        <img
-                            className="rounded-lg"
-                            src="/images/project2.png"
-                            alt="Popcorn Picker App"
-                        />
-                    </div>
-                        <h2>Movie Rating App</h2>
-                </div>
+            <div className="text-content">
+              <h2>
+                Business Website with a Simple yet Elegant Design and Focussed
+                on Ease of Use.
+              </h2>
+              <p className="text-white-50 text-xl">
+                A Website Built with Next.js, TypeScript, Tailwind CSS, GSAP,
+                next-intl. (Test Phase Deployment)
+              </p>
+              <p>
+                <a
+                  href="https://anker-tattoo-piercing-freiburg.onrender.com/en"
+                  target="_blank"
+                >
+                  Check it out!
+                </a>
+              </p>
+            </div>
+          </div>
+          {/** RIGHT */}
+          <div className="project-list-wrapper overflow-hidden">
+            <div className="project" ref={project2Ref}>
+              <div className="image-wrapper bg-[rgb(23,23,25)]">
+                <img
+                  className="rounded-lg"
+                  src="/images/project2.png"
+                  alt="Popcorn Picker App"
+                />
+              </div>
+              <h2>Movie Rating App</h2>
+            </div>
 
-                <div className="project" ref={project3Ref}>
-                    <div className="image-wrapper bg-[rgb(23,23,25)]">
-                        <img
-                            className=""
-                            src="/images/project3.png"
-                            alt="Chef Ollama App"
-                        />
-                    </div>
-                        <h2>AI Gen Recipes</h2>
-                </div>
+            <div className="project" ref={project3Ref}>
+              <div className="image-wrapper bg-[rgb(23,23,25)]">
+                <img
+                  className=""
+                  src="/images/project3.png"
+                  alt="Chef Ollama App"
+                />
+              </div>
+              <h2>AI Gen Recipes</h2>
             </div>
+          </div>
         </div>
-        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Showcase
+export default Showcase;
